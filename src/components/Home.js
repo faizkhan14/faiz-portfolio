13dfad08d7a9d983354ab0f75fe569f5804a3899
Mohd Faiz Khan <mohdfaizk144@gmail.com>
2024-08-27 14:53:04 +0530

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ return (
                 As a passionate front-end developer, I specialize in crafting visually appealing and user-friendly web interfaces. With a strong foundation in HTML, CSS, JavaScript, ReactJS, Bootstrap, and Flutter, I bring a diverse skill set to every project. My recent internship at Techweu provided valuable hands-on experience, solidifying my understanding of industry best practices and enabling me to contribute effectively to collaborative development teams.
             </div>
             
-            <a href="/democv.pdf" target="_blank" rel="noopener noreferrer" className='animation active '>
+            <a href="/Faiz Resume.pdf" target="_blank" rel="noopener noreferrer" className='animation active '>
              My CV
             </a>
         </div>
