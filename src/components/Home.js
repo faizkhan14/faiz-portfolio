db7b958cee964a5f84546ac68bfbaaf855d2e43e
Mohd Faiz Khan <mohdfaizk144@gmail.com>
2024-09-26 22:35:56 +0530

    avatar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,32 @@
-import React, { useRef } from 'react' 
+import React, { useRef } from 'react';
 import CustomHook from './CustomHook';
-function Home() {
-const scrollTab = useRef();
-CustomHook(scrollTab);
+import avatar from './images/avatar.png';
 
-return (
-    <section ref={scrollTab} className='home'>
-        <div className="content">
-            <div className="name">
-                My Name Is <span>FAIZ</span>
+function Home() {
+    const scrollTab = useRef();
+    CustomHook(scrollTab);
+
+    return (
+        <section ref={scrollTab} className='home'>
+            <div className="content">
+                <div className="name">
+                    My Name Is <span>FAIZ</span>
+                </div>
+                <div className="des">
+                    As a passionate front-end developer, I specialize in crafting visually appealing and user-friendly web interfaces. With a strong foundation in HTML, CSS, JavaScript, [ReactJS](https://react.dev/), [Bootstrap](https://getbootstrap.com/), and Flutter, I bring a diverse skill set to every project. My recent internship at Techweu provided valuable hands-on experience, solidifying my understanding of industry best practices and enabling me to contribute effectively to collaborative development teams.
+                </div>
+                
+                <a href="./FaizResume.pdf" target="_blank" rel="noopener noreferrer" className='animation active'>
+                    My CV
+                </a>
             </div>
-            <div className="des">
-                {/* 30 */}
-                As a passionate front-end developer, I specialize in crafting visually appealing and user-friendly web interfaces. With a strong foundation in HTML, CSS, JavaScript, ReactJS, Bootstrap, and Flutter, I bring a diverse skill set to every project. My recent internship at Techweu provided valuable hands-on experience, solidifying my understanding of industry best practices and enabling me to contribute effectively to collaborative development teams.
+            <div className="avatar">
+                <div className="card">
+                    <img src={avatar} alt="Avatar" />
+                </div>
             </div>
-            
-            <a href="/Faiz Resume.pdf" target="_blank" rel="noopener noreferrer" className='animation active '>
-             My CV
-            </a>
-        </div>
-        <div className="avatar">
-            <div className="card">
-                <img src="./public/avatar.png" alt="" />
-            </div>
-        </div>
-    </section>
-    )
+        </section>
+    );
 }
 
-export default Home
+export default Home;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,72 +1,64 @@
-import React, { useState, useRef } from 'react' 
+import React, { useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPersonCircleQuestion, faEarthAmericas } from '@fortawesome/free-solid-svg-icons';
+import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons';
 import CustomHook from './CustomHook';
+import project1 from './images/project1.png';
+import project2 from './images/project2.png';
+import project3 from './images/project3.png';
 
 function Projects() {
-const [listProjects] = useState([
-  {
-    name: 'Snapper Website',
-    des: 'Eu voluptate sit do labore consectetur in ad esse qui laborum ad eiusmod. Esse ea velit culpa exercitation anim enim reprehenderit. Fugiat nostrud non dolore aliquip quis in ea amet duis.',
-    mission: 'Back-end Developer, system analysis and design',
-    language: 'HTML5, CSS3, JavaScript, Bootstrap',
-    images: '/project1.png'
-  },
-  {
-    name: 'Zoom Clone',
-    des: 'Eu voluptate sit do labore consectetur in ad esse qui laborum ad eiusmod. Esse ea velit culpa exercitation anim enim reprehenderit. Fugiat nostrud non dolore aliquip quis in ea amet duis.',
-    mission: 'Back-end Developer, system analysis and design',
-    language: 'Flutter',
-    images: '/project2.png'
-  },
-  {
-    name: 'Portfolio using React',
-    des: 'Eu voluptate sit do labore consectetur in ad esse qui laborum ad eiusmod. Esse ea velit culpa exercitation anim enim reprehenderit. Fugiat nostrud non dolore aliquip quis in ea amet duis.',
-    mission: 'Back-end Developer, system analysis and design',
-    language: 'React JS',
-    images: '/project3.png'
-  },
+  const [listProjects] = useState([
+    {
+      name: 'Snapper Website',
+      des: 'Designed and developed a responsive website for Snapper Energy Drink using HTML, CSS, JavaScript, and Bootstrap.',
+      language: 'HTML5, CSS3, JavaScript, Bootstrap',
+      images: project1
+    },
+    {
+      name: 'Zoom Clone',
+      des: 'Developed a Zoom clone application using Flutter, replicating key features such as video conferencing, chat functionality, and user authentication.',
+      language: 'Flutter',
+      images: project2
+    },
+    {
+      name: 'Portfolio using React',
+      des: 'Developed a dynamic and interactive personal portfolio website using ReactJS, showcasing my skills, projects, and experiences.',
+      language: 'React JS',
+      images: project3
+    },
+  ]);
 
-]);
-const divs = useRef([]);
-const scrollTab = useRef();
-CustomHook(scrollTab, divs);
+  const divs = useRef([]);
+  const scrollTab = useRef();
+  CustomHook(scrollTab, divs);
+
   return (
     <section className='projects' ref={scrollTab}>
-       <div className="title" ref={(el) => el && divs.current.push(el)}>
+      <div className="title" ref={(el) => el && divs.current.push(el)}>
         My Projects
-       </div>
-       
-       <div className="list">
-        {
-          listProjects.map((value, key) => (
-            <div className='item' key={key} ref={(el) => el && divs.current.push(el)}>
-              <div className="images">
-                <img src={value.images} alt="" />
-              </div>
-              <div className="content">
-                <h3>{value.name}</h3>
-                <div className="des">{value.des}</div>
-                <div className="mission">
-                    <div><FontAwesomeIcon icon={faPersonCircleQuestion} /></div>
-                    <div>
-                        <h4>Mission</h4>
-                        <div className="des">{value.mission}</div>
-                    </div>
-                </div>
-                <div className="mission">
-                    <div><FontAwesomeIcon icon={faEarthAmericas} /></div>
-                    <div>
-                        <h4>Languages</h4>
-                        <div className="des">{value.language}</div>
-                    </div>
+      </div>
+      <div className="list">
+        {listProjects.map((value, key) => (
+          <div className='item' key={key} ref={(el) => el && divs.current.push(el)}>
+            <div className="images">
+              <img src={value.images} alt={value.name} />
+            </div>
+            <div className="content">
+              <h3>{value.name}</h3>
+              <div className="des">{value.des}</div>
+              <div className="mission">
+                <div><FontAwesomeIcon icon={faEarthAmericas} /></div>
+                <div>
+                  <h4>Languages</h4>
+                  <div className="des">{value.language}</div>
                 </div>
               </div>
             </div>
-          ))
-        }
-       </div>
+          </div>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
-export default Projects
+
+export default Projects;
